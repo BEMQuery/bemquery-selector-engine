edf6cd2681afb1ac2c9abfed059f4ee26d581fc4
Comandeer <vepomoc@gmail.com>
2016-04-12 23:08:56 +0200

    [ci skip] Changed UMD module global variable name.

--- config/rollup/umd.js
+++ config/rollup/umd.js
@@ -1,7 +1,7 @@
 import config from './es6.js';
 
 config.format = 'umd',
-config.moduleName = 'bemquery-selector-engine'
-config.dest = 'dist/bemquery-selector-engine.umd.js'
+config.moduleName = 'bemquerySelectorEngine';
+config.dest = 'dist/bemquery-selector-engine.umd.js';
 
 export default config;
