33b863dc09982a3a9f99510b38e97db3e2b04289
Comandeer <vepomoc@gmail.com>
2016-04-13 18:58:41 +0200

    [ci skip] Added Tonic example.

---
+++ tonic-example.js
@@ -0,0 +1,28 @@
+require( 'bemquery-selector-engine/package.json' );
+
+var fs = require( 'fs' ),
+	url = 'https://tonicdev.io' + process.env.TONIC_ENDPOINT_PATH;
+
+exports.tonicEndpoint = function( request, response ) {
+	response.end( fs.readFileSync( require.resolve( 'bemquery-selector-engine' ) ) );
+};
+
+`<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>Tonic Example</title>
+		</head>
+		<body>
+			<p class="block">
+				<span class="block__elem block__elem_modifier">Example</span>
+			</p>
+			<script src="${url}"></script>
+			<script>
+			var engine = bemquerySelectorEngine.factory(),
+				span = engine.find( 'block elem:modifier' ).elements[ 0 ];
+
+				span.style.color = '#f00';
+			</script>
+		</body>
+	</html>`;
