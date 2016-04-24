dbb8222a14f3f0e8c5161ff5f4d70615cd32c648
Comandeer <vepomoc@gmail.com>
2016-04-24 17:05:54 +0200

    Made factory a default module's export and added Selector to the exports list.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
 export { default as Converter } from './converter/Converter';
+export { default as Selector } from './converter/Selector';
 export { default as SelectorEngine } from './SelectorEngine';
-export { default as factory } from './factory';
+export { default as default } from './factory';
--- tests/module.js
+++ tests/module.js
@@ -3,19 +3,30 @@
 'use strict';
 
 import * as bse from '../src/index';
+import bse2 from '../src/index';
+
+import factory from '../src/factory';
+import SelectorEngine from '../src/SelectorEngine';
+import Converter from '../src/converter/Converter';
+import Selector from '../src/converter/Selector';
 
 const expect = chai.expect;
 
 describe( 'module', () => {
 	it( 'exports factory', () => {
-		expect( bse.factory ).to.be.a( 'function' );
+		expect( bse.default ).to.equal( factory );
+		expect( bse2 ).to.equal( factory );
 	} );
 
 	it( 'exports SelectorEngine', () => {
-		expect( bse.SelectorEngine ).to.be.an( 'function' );
+		expect( bse.SelectorEngine ).to.equal( SelectorEngine );
 	} );
 
 	it( 'exports Converter', () => {
-		expect( bse.Converter ).to.be.an( 'function' );
+		expect( bse.Converter ).to.equal( Converter );
+	} );
+
+	it( 'exports Selector', () => {
+		expect( bse.Selector ).to.equal( Selector );
 	} );
 } );
