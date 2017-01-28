6d30d0e7163a2bdaf76d2edfa64b814ef87753fb
Tomasz Jakut <vepomoc@gmail.com>
2017-01-28 20:13:59 +0100

    chore(dist): clean after release [ci skip]

--- dist/bemquery-selector-engine.js
+++
@@ -1,2 +0,0 @@
-/*! bemquery-selector-engine v0.2.5 | (c) 2016-2017 BEMQuery team | MIT license (see LICENSE) */class SelectorEngine{find(a,b=document){let c=!1;b!==document&&(!b.id&&(c=!0,b.id=`BEMQueryTMP_${Date.now()}`),a=`#${b.id} ${a}`);const d=Array.from(b.querySelectorAll(a));return c&&b.removeAttribute('id'),d}}export default SelectorEngine;
-//# sourceMappingURL=bemquery-selector-engine.js.map
